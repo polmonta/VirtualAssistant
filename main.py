3462e4f9dc7292494b21275566198db1a0382c7d
polmv123 <mrgoma8@gmail.com>
2021-02-03 21:17:10 -0500

    [🔧] Minor improvements

--- main.py
+++ main.py
@@ -4,38 +4,48 @@ import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
+import weather
+from time import sleep
+from word2number import w2n
+
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-name = 'Alexa'
+name = 'alexa'
 name = name.lower()
 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-    
+
 def listenInput():
     command = False
-    try:
-        with sr.Microphone() as source:
-            print('Listening...')
-            voice = listener.listen(source)
-            audio = listener.recognize_google(voice)
-            audio = audio.lower()
-            print(audio)
-            if name in audio:
-                command = audio.replace(name, '')
-                print('listeningInput= ' + command)
-                return command
-            else:
-                pass
-    except:
-        print('Wake word not detecting.')
-    return command
+    listening = True
+    if listening is True:
+        try:
+            with sr.Microphone() as source:
+                listener.adjust_for_ambient_noise(source)
+                print('Listening...')
+                voice = listener.listen(source)
+                audio = listener.recognize_google(voice)
+                audio = audio.lower()
+                print(audio)
+                if name in audio:
+                    command = audio.replace(name, '')
+                    print('listeningInput= ' + command)
+                    return command
+                else:
+                    pass
+        except:
+            print('Wake word not detected.')
+        return command
+    else:
+        pass
+
 
 def executeOutput():
     command = listenInput()
@@ -59,6 +69,20 @@ def executeOutput():
             talk('I am your private assistant')
         elif 'tell me something fun' in command:
             talk(pyjokes.get_joke())
+        elif 'weather' in command:
+            city = command.replace("what's the weather in", '')
+            temperature = weather.temperature(city)
+            feel_like = weather.feels_like(city)
+            country = weather.country(city)
+            temp_min = weather.temp_min(city)
+            temp_max = weather.temp_max(city)
+            basic = weather.basic(city)
+            print('Current forecast in ' + str(city) + ","  + str(country) +  ' is ' + str(basic) + ". The temperature now is about "+ str(temperature) + 
+            ' degrees. Maximum temperature for today will be ' + str(temp_max) + ' degrees, and lower ' + str(temp_min) + " degrees fahrenheit")
+            talk('Current forecast in ' + str(city) + ","  + str(country) +  ' is ' + str(basic) +  ". The temperature now is about "+ str(temperature) + 
+            ' degrees. Maximum temperature for today will be ' + str(temp_max) + ' degrees, and lower ' + str(temp_min) + " degrees fahrenheit")
+
+
         else:
             if 'what is' in command:
                 googleSearch = command.replace('what is', '')
